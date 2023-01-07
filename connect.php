ca75432fcbae60d0bbf18cc45e46cae358b0d878
krxtsxdx <kritsada.buts@ovec.moe.go.th>
2023-01-07 11:10:50 +0700

    connect

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
     $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "web2111";
-    // $username = "it65309010103";
-    // $password = "it12345";
-    // $dbname = "it65309010103";
+    // $username = "root";
+    // $password = "";
+    // $dbname = "web2111";
+    $username = "it65309010103";
+    $password = "it12345";
+    $dbname = "it65309010103";
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
